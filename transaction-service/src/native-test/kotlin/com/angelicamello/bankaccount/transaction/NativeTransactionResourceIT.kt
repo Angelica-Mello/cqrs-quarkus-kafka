04a06688e78e99f3f49a7fe1bc784833a652f10e
Angélica Mello <angelicamoliveira@hotmail.com>
2021-04-18 21:55:12 -0300

    cqrs-quarkus-kafka

---
+++ transaction-service/src/native-test/kotlin/com/angelicamello/bankaccount/transaction/NativeTransactionResourceIT.kt
@@ -0,0 +1,6 @@
+package com.angelicamello.bankaccount.transaction
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+open class NativeTransactionResourceIT : TransactionResourceTest()
